cec042898804f82e760441046c63c6a1fea97ae0
Olivia Golden <olivia.golden.w@gmail.com>
2023-12-14 14:20:03 -0600

    linked list class functions rough draft, will come back after node/jest in the curriculum

--- linked-list.js
+++ linked-list.js
@@ -1,76 +1,181 @@
 /** Node: node for a singly linked list. */
-
 class Node {
-  constructor(val) {
-    this.val = val;
-    this.next = null;
-  }
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
 }
 
 /** LinkedList: chained together nodes. */
-
 class LinkedList {
-  constructor(vals = []) {
-    this.head = null;
-    this.tail = null;
-    this.length = 0;
+    constructor(vals = []) {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
 
-    for (let val of vals) this.push(val);
-  }
+        for (let val of vals) this.push(val);
+    }
 
-  /** push(val): add new value to end of list. */
+    /** push(val): add new value to end of list. */
+    push(val) {
+        const node = new Node(val);
 
-  push(val) {
+        if (this.head === null) this.head = node;
 
-  }
+        if (this.tail !== null) this.tail.next = node;
 
-  /** unshift(val): add new value to start of list. */
+        this.tail = node;
+    }
 
-  unshift(val) {
+    /** unshift(val): add new value to start of list. */
+    unshift(val) {
+        const node = new Node(val);
 
-  }
+        if (this.head === null) {
+            this.head = node;
+        }
+        else {
+            this.head = this.head.next;
+            this.head = node;
+        }
+    }
 
-  /** pop(): return & remove last item. */
+    /** return previous node to the given search value */
+    findPrevious(val) {
+        let current = this.head;
+        let previous = null;
 
-  pop() {
+        while (current !== null) {
+            if (current.val === val) return previous;
 
-  }
+            previous = current;
+            current = current.next;
+        }
 
-  /** shift(): return & remove first item. */
+        return previous;
+    }
 
-  shift() {
+    /** pop(): return & remove last item. */
+    pop() {
+        const last = this.tail;
 
-  }
+        let secondToLast = this.findPrevious(last);
+        secondToLast.next = null;
 
-  /** getAt(idx): get val at idx. */
+        return last.val;
+    }
 
-  getAt(idx) {
+    /** shift(): return & remove first item. */
+    shift() {
+        const first = this.head;
+        const second = first.next;
+        this.head = second;
 
-  }
+        return first;
+    }
 
-  /** setAt(idx, val): set val at idx to val */
+    /** getAt(idx): get val at idx. */
+    getAt(idx) {
+        if (idx < 0) {
+            throw Error;
+        }
 
-  setAt(idx, val) {
+        let count = 0;
+        let current = this.head;
 
-  }
+        while (count < idx) {
+            if (current === null) {
+                throw Error;
+            }
+            count += 1;
+            current = current.next;
+        }
 
-  /** insertAt(idx, val): add node w/val before idx. */
+        return current.vl
+    }
 
-  insertAt(idx, val) {
+    /** setAt(idx, val): set val at idx to val */
+    setAt(idx, val) {
+        if (idx < 0) {
+            throw Error;
+        }
 
-  }
+        let count = 0;
+        let current = this.head;
 
-  /** removeAt(idx): return & remove item at idx, */
+        while (count < idx) {
+            if (current === null) {
+                throw Error;
+            }
+            count += 1;
+            current = current.next;
+        }
 
-  removeAt(idx) {
+        current.val = val;
+    }
 
-  }
+    /** insertAt(idx, val): add node w/val before idx. */
+    insertAt(idx, val) {
+        if (idx < 0) {
+            throw Error;
+        }
 
-  /** average(): return an average of all values in the list */
+        let count = 0;
+        let current = this.head;
+        let previous = null;
 
-  average() {
-    
-  }
+        while (count < idx) {
+            if (current === null) {
+                throw Error;
+            }
+            count += 1;
+            previous = current;
+            current = current.next;
+        }
+
+        const node = new Node(val);
+        node.next = current;
+        previous.next = node;
+    }
+
+    /** removeAt(idx): return & remove item at idx, */
+    removeAt(idx) {
+        if (idx < 0) {
+            throw Error;
+        }
+
+        let count = 0;
+        let current = this.head;
+        let previous = null;
+
+        while (count < idx) {
+            if (current === null) {
+                throw Error;
+            }
+            count += 1;
+            previous = current;
+            current = current.next;
+        }
+
+        previous.next = current.next;
+        return current.val;
+    }
+
+    /** average(): return an average of all values in the list */
+    average() {
+        const last = this.tail;
+        let sum = null;
+        let count = 0;
+        let current = this.head;
+
+        while (current !== last) {
+            sum += current.val;
+            count += 1;
+            current = current.next;
+        }
+
+        return (sum / count);
+    }
 }
 
 module.exports = LinkedList;
